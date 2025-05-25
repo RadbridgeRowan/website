d28ee7936ab386a888f64ea1536e2395508a4df6
RadbridgeRowan <129906364+RadbridgeRowan@users.noreply.github.com>
2025-05-25 15:11:35 +1200

    Update Vite base path for new repository name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/radbridge-website/',
+  base: '/website/',
   // You can add other Vite options here if needed
 }); 

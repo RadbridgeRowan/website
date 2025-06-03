9d818dcfef58f92d3a5126697bd10c17ce506278
RadbridgeRowan <129906364+RadbridgeRowan@users.noreply.github.com>
2025-06-03 21:26:44 +1200

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     "./about.html",
     "./services.html",
     "./projects.html",
+    "./grantspan.html",
+    "./parkbeat.html",
+    "./tools.html",
+    "./projects-test.html",
     "./src/**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
@@ -12,11 +16,17 @@ export default {
       colors: {
         'radbridge-blue': '#28506C', // Primary
         'radbridge-gold': '#D19B46', // Secondary
-        primary: '#28506C',
-        secondary: '#D19B46',
+        'light-gold': '#EAD6A8',
+        'light-grey': '#F4F4F4', 
+        'dark-blue': '#062941',
+        'black': '#0A0B0B',
       },
       fontFamily: {
+        ibm: ['IBM Plex Sans', 'sans-serif'],
         sans: ['"Public Sans"', 'sans-serif'],
+        inter: ['Inter', 'system-ui', 'sans-serif'],
+        'source-sans': ['"Source Sans Pro"', 'Arial', 'sans-serif'],
+        lato: ['Lato', 'Helvetica', 'sans-serif'],
       },
       container: {
         center: true,
